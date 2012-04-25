0bffe3bce9e7d5b6f8a7603f43b3f03127817217
Matthew Keating <matthewkeating007@gmail.com>
2012-04-24 23:57:02 -0400

    Init Upload

---
+++ Axis.py
@@ -0,0 +1,37 @@
+from visual import *
+x = 0
+y = 0
+z = 0
+
+yaxistop = arrow(pos=(0,0,0), axis=(0,100,0), shaftwidth=.2,color=color.blue)
+yaxisbottom = arrow(pos=(0,0,0), axis=(0,-100,0), shaftwidth=.2,color=color.blue)
+xaxisright = arrow(pos=(0,0,0), axis=(100,0,0), shaftwidth=.2,color=color.red)
+xaxisleft = arrow(pos=(0,0,0), axis=(-100,0,0), shaftwidth=.2,color=color.red)
+ball = sphere(pos=(x,y,z), radius=5, color=color.green)
+label(pos=(-75,20,0), text='X-Axis')
+label(pos=(25,80,0), text='Y-Axis')
+
+
+while True:
+    rate(100)
+    if scene.kb.keys: # is there an event waiting to be processed?
+        s = scene.kb.getkey() # obtain keyboard information
+    
+        if s == 'up':
+            y = y + 3
+            ball.pos = (x,y,z)
+
+        if s == 'down':
+            y = y - 3 
+            ball.pos = (x,y,z)
+
+        if s == 'right':
+            x = x + 3
+            ball.pos = (x,y,z)
+        if s == 'left':
+            x = x - 3
+            ball.pos = (x,y,z)
+            
+        loc = ball.pos
+        locstr = str(loc)
+        label(pos=(60,-70,0), text=locstr,box=false)
